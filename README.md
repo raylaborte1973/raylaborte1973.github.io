8f9f32e954fb0411bfdd18d165ad91ad9e946c23
raylaborte1973 <raylaborte1973@uspf.edu.ph>
2021-12-04 23:01:54 +0800

    added new

--- README.md
+++
@@ -1,1 +0,0 @@
-# raylaborte1973.github.io
--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-  }
-   
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
+
+// Redirect image 1 to another page
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('#vid1').onclick = function () {
+        window.open('https://www.youtube.com/watch?v=2lY-wU0PXeA', '_blank')
+    }
+});
+
+// Redirect image 2 to another page
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('#vid2').onclick = function () {
+        window.open('https://www.youtube.com/watch?v=MyTMVMGgwNg&t=146s', '_blank')
+    }
+});
+
+// Redirect image 3 to another page
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('#vid3').onclick = function () {
+        window.open('https://www.youtube.com/watch?v=4vqOqwOUIpY', '_blank')
+    }
+});
+
+// Redirect image 4 to another page
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('#vid4').onclick = function () {
+        window.open('https://www.youtube.com/watch?v=yruezOI0PGc&t=46s', '_blank')
+    }
+});
+
